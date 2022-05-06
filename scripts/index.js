6db237943e0eff8a14e7cc629ffd6d6de71d61e2
SergioLucasINT <99209004+SergioLucasINT@users.noreply.github.com>
2022-05-06 09:10:22 -0300

    Dia 05/05/2022

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,20 @@
-let funcYamaha = document.getElementById('func');
+//PEDRO
+let botaoSim =  document.getElementById('btnradio1');
+let botaoNao = document.getElementById('btnradio2');
 
+let tipo1 = document.getElementById('labelTipo1');
+let tipo2 = document.getElementById('labelTipo2');
 
-funcYamaha.addEventListener('click', () =>{
-    if(funcYamaha.checked){
-        alert('Olá');
+botaoSim.addEventListener('click', () =>{
+    if(botaoSim.checked){
+        tipo1.innerHTML = 'Registro';
+        tipo2.innerHTML = 'Governança';
+    }
+});
+
+botaoNao.addEventListener('click', () =>{
+    if(botaoNao.checked){
+        tipo1.innerHTML = 'Empresa';
+        tipo2.innerHTML = 'Duração do contrato';
     }
 });
