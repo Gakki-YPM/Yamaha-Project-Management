26edcf4718a63ab35305dce618d9a75709051f0a
pedrogfsilva <pedro.silva@sou.inteli.edu.br>
2022-06-01 10:01:18 -0300

    formulario funcionando

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const projectsRoutes = require('./routes/projects');
 const employeesRoutes = require('./routes/employees');
+//const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
+// app.get('/', (req, res) =>{
+//     res.render('index');
+// });
+
+app.use(express.static('../frontend'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/projects', projectsRoutes);
 app.use('/employees', employeesRoutes);
 
