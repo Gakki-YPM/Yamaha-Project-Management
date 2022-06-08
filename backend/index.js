a1087830c2958aa86460df6d56710544141c6f79
SergioLucasINT <99209004+SergioLucasINT@users.noreply.github.com>
2022-06-08 14:14:47 -0300

    21312
    32131232

--- backend/index.js
+++ backend/index.js
@@ -21,5 +21,5 @@ app.use('/governance', governanceRoutes);
 app.use('/role', roleRoutes);
 
 app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port localhost:${PORT}`);
 });
