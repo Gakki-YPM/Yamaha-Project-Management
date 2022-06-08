b927b0758cb39322a185eae00059adb2f83c7b35
SergioLucasINT <99209004+SergioLucasINT@users.noreply.github.com>
2022-06-08 09:09:57 -0300

    osakdoask
    osakdoask

--- scripts/index.js
+++ scripts/index.js
@@ -80,3 +80,92 @@ if (document.title === 'Cadastrar Funcionário')
             document.getElementById('final').disabled = false
         }
     }
+
+    /* ATRIBUIR FUNCIONÁRIOS*/
+    // Drag Tasks
+
+const todos = document.querySelectorAll('.todo'); 
+const all_status = document.querySelectorAll('.status');
+let draggableTodo = null; 
+
+todos.forEach(
+    (todo) => {
+        todo.addEventListener('dragstart', dragStart);
+        todo.addEventListener('dragend', dragEnd);
+    });
+
+function dragStart(){
+    draggableTodo = this; 
+    console.log('Drag Start');
+    setTimeout(() => {
+        this.style.display = 'none'; 
+    }, 0);
+}
+
+function dragEnd(){
+    draggableTodo = null; 
+    console.log('Drag End');
+    setTimeout(() => {
+        this.style.display = 'block'; 
+    }, 0);
+}
+
+all_status.forEach(status => {
+    status.addEventListener('dragover', dragOver);
+    status.addEventListener('dragenter', dragEnter);
+    status.addEventListener('dragleave', dragLeave);
+    status.addEventListener('drop', dragDrop);
+}); 
+
+function dragOver(e){
+    e.preventDefault(); 
+    console.log('Drag Over');
+}
+
+function dragEnter(){
+    this.style.border = '1px dashed #ccc';
+    console.log('Drag Enter');
+}
+
+function dragLeave(){
+    this.style.border = 'none';
+    console.log('Drag Leave');
+}
+
+function dragDrop(){
+    this.style.border = 'none';
+    this.appendChild(draggableTodo);
+    console.log('Drag Drop');
+}
+
+// Modal 
+
+const btns = document.querySelectorAll("[data-target-modal]");
+const close_modals = document.querySelectorAll(".modal-btn"); 
+const overlay = document.querySelector("#overlay");
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector(btn.dataset.targetModal).classList.add('active');
+        overlay.classList.add("active");
+    })
+});
+
+close_modals.forEach(btn => {
+    btn.addEventListener('click', () => {
+        //Duas maneiras de remover o Modal.
+        document.querySelector(btn.dataset.targetModal).classList.remove('active');
+        // btn.closest(".modal").classList.remove("active"); 
+        overlay.classList.remove("active"); 
+    })
+});
+
+window.onclick = (e) => {
+    if (e.target == overlay) {
+        const modals = document.querySelectorAll('.modal');
+        modals.forEach(modal => modal.classList.remove('active'));
+        overlay.classList.remove("active");  
+    }
+}
+
+
